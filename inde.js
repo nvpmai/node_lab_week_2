3edf1ae4fb83dc51a8c6836cea21fb5114391cbe
HieuNM6 <hieunmce@gmail.com>
2016-10-20 19:51:06 +0700

    Milstone 2

--- inde.js
+++ inde.js
@@ -3,9 +3,10 @@
 require('./helper')
 const fs = require('fs').promise
 const express = require('express')
-const promiseRouter = require('express-promise-router')
+// const promiseRouter = require('express-promise-router')
 const morgan = require('morgan')
 const trycatch = require('trycatch')
+const path = require('path')
 // const bodyParser = require('body-parser')
 
 // let app = express()
@@ -13,6 +14,17 @@ const trycatch = require('trycatch')
 // if (NODE_ENV === 'development') {
 //   app.use(morgan('dev'))
 // }
+async function read(req, res) {
+  const filePath = path.join(__dirname, 'files', req.url)
+  const data = await fs.readFile(filePath).catch((err) => {
+    if (err.code === 'EISDIR') {
+      res.end("Can't read directory");
+    }
+  });
+  if (data) {
+    res.end(data)
+  }
+}
 
 async function initialize() {
   const app = express()
@@ -26,8 +38,11 @@ async function initialize() {
   })
 
   const port = 8000
+  app.get('*', read)
+
   app.listen(port);
   console.log(`LISTENING @ http://127.0.0.1:${port}`)
 }
+
 
 initialize()
